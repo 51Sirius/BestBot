f92ad0ff9881c07a325e66d6100da4f742325011
51Sirius <71227522+51sirius@users.noreply.github.com>
2023-08-01 14:53:27 +0300

    Transport admin functions from ranks.py to admin.py

--- src/admin.py
+++ src/admin.py
@@ -1,5 +1,7 @@
 import disnake
 import cfg
+import src.card.card as card
+from src.main import *
 
 from disnake.ext import commands
 
@@ -8,9 +10,42 @@ class Admin(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.slash_command(name='echo', aliases=['echo'], description="Responds with 'World'")
-    async def echo(self, ctx):
-        print(ctx.options)
+    @commands.has_role('Глава секты')
+    @commands.slash_command(name='transport', aliases=['trans'],
+                            description="Перенесет культивацию с одного человека другому")
+    async def transport(self, inter, from_member: disnake.Member, to_member: disnake.Member):
+        score = give_point_from_user(from_member.id)
+        cult, update = add_point(to_member.id, score)
+        clear_cult(from_member)
+        if update:
+            await give_role_with_cult(to_member, cult)
+        await inter.send('Transfer is successfully')
+
+    @commands.has_role('Глава секты')
+    @commands.slash_command(name='add_point',
+                            description="Добавляет очки")
+    async def add_point(self, inter, to_member: disnake.Member = None, value=0):
+        if to_member is not None:
+            cult, update = add_point(to_member.id, value)
+            if update:
+                await give_role_with_cult(to_member, cult)
+            await inter.send('Add points to member')
+        else:
+            cult, update = add_point(inter.author.id, int(value))
+            if update:
+                await give_role_with_cult(inter.author, cult)
+            await inter.send('Add point')
+
+    @commands.has_role('Глава секты')
+    @commands.slash_command(name='how_many_dao',
+                            description="Сколько духовной силы у человека")
+    async def how_many_dao(self, inter, member: disnake.Member = None):
+        if member is not None:
+            score = give_point_from_user(member.id)
+            await inter.send(f'User {member.name} has {score}')
+        else:
+            score = give_point_from_user(inter.author.id)
+            await inter.send(f'You have {score}')
 
 
 def setup(bot):
